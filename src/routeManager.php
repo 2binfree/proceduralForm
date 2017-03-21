284c4d125c363ed38f4a54954c902cbb019a7775
Laurent Aubertin <2binfree@gmail.com>
2017-03-21 09:28:34 +0100

    WIP router refactoring

--- src/routeManager.php
+++ src/routeManager.php
@@ -3,28 +3,59 @@
 /**
  * @param $uri
  * @return array
+ * @throws HttpException
  */
 function getRoute($uri) {
 
     require '../config/routes.php';
 
     $segments = explode('?', $uri);
-    $pages = explode('/', $segments[0]);
-    if (isset($routes[$pages[1]])){
-        $index = $pages[1];
-    } else {
-        $index = "index";
+    $arguments = $segments[1];
+    $parameters = array();
+
+    $path = explode('/', $segments[0]);
+
+    // get main page to display
+    array_shift($path);
+    $queryPage = "index";
+    if (isset($path[0])){
+        $queryPage = $path[0];
+        if (!isset($routes[$queryPage])){
+            header("HTTP/1.0 404 Not Found");
+        }
     }
-    $argument = (isset($pages[2])) ? $pages[2] : "";
-    $page = $routes[$index]['page'];
-    $title = $routes[$index]['title'];
+
+    // parse all argument passed by slashed path
+    array_shift($path);
+    $target = "name";
+    $parameterName = "";
+    foreach($path as $arg){
+        if ($target === "name"){
+            $target = "value";
+            $parameterName = $arg;
+        } else {
+            // target = value
+            $parameters[$parameterName] = $arg;
+            $target = "name";
+        }
+    }
+
+    // parse all argument passed by classic way (? and &)
+    $args = explode("&", $arguments);
+    foreach($args as $arg){
+        $elements = explode("=", $arg);
+        $parameters[$elements[0]] = $elements[1];
+    }
+
+    $page = $routes[$queryPage]['page'];
+    $title = $routes[$queryPage]['title'];
     $file = $page . '.php';
 
     return array(
-        'page'      => $page,
-        'title'     => $title,
-        'file'      => $file,
-        'argument'  => $argument,
+        'page'          => $page,
+        'title'         => $title,
+        'file'          => $file,
+        '$parameters'   => $parameters,
     );
 }
 
