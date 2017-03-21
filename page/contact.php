690f86d57d0e0d033c81ccb11c4ad9dec3dd4609
Laurent Aubertin <2binfree@gmail.com>
2017-03-21 13:37:23 +0100

    add hydrator and code refactoring

--- page/contact.php
+++ page/contact.php
@@ -1,51 +1,43 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $userManager->setFirstName($_POST["firstname"]);
-        $userManager->setLastname($_POST["lastname"]);
-        $userManager->setEmail($_POST["email"]);
-        $userManager->setPassword($_POST["password"]);
-        if ($_POST["id"] !== ""){
-            $userManager->updateUser($_POST["id"]);
+        $userManager->hydrate($_POST);
+        if ($userManager->getId() !== ""){
+            $userManager->updateUser();
         } else {
             $userManager->addUser();
         }
         header('Location: /?page=index&addcontact=ok');
     } else {
-        if (isset($_GET['userid'])) {
-            $id = $_GET['userid'];
-            $user = $userManager->getUser($id);
-            $firstname =  $user['firstname'];
-            $lastname =  $user['lastname'];
-            $email =  $user['email'];
-            $password =  $user['password'];
+        if (isset($_GET['id'])) {
+            $userManager->getUser($_GET['id']);
         }
 ?>
 <div class="row">
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"></div>
     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
         <form action="" method="post" role="form">
-            <input type="hidden" name="id" value="<?php echo $id;?>">
+            <input type="hidden" name="id" value="<?php echo $userManager->getId();?>">
             <legend>Formulaire de contact</legend>
 
             <div class="form-group">
                 <label for="">Prénom</label>
                 <input type="text" class="form-control" name="firstname" id="firstname"
-                       placeholder="Entrez votre prénom" value="<?php echo $firstname;?>">
+                       placeholder="Entrez votre prénom" value="<?php echo $userManager->getFirstName();?>">
             </div>
             <div class="form-group">
                 <label for="">Nom</label>
                 <input type="text" class="form-control" name="lastname" id="lastname"
-                       placeholder="Entrez votre nom" value="<?php echo $lastname;?>">
+                       placeholder="Entrez votre nom" value="<?php echo $userManager->getLastName();?>">
             </div>
             <div class="form-group">
                 <label for="">Email</label>
                 <input type="text" class="form-control" name="email" id="email"
-                       placeholder="Entrez votre email" value="<?php echo $email;?>">
+                       placeholder="Entrez votre email" value="<?php echo $userManager->getEmail();?>">
             </div>
             <div class="form-group">
                 <label for="">Mot de passe</label>
                 <input type="text" class="form-control" name="password" id="password"
-                       placeholder="Entrez votre mot de passe" value="<?php echo $password;?>">
+                       placeholder="Entrez votre mot de passe" value="<?php echo $userManager->getPassword();?>">
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
--- src/UserManager.php
+++ src/UserManager.php
@@ -19,6 +19,10 @@ class UserManager
     private $email;
     private $password;
 
+    /**
+     * @var int
+     */
+    private $id;
 
     /**
      * UserManager constructor.
@@ -27,6 +31,28 @@ class UserManager
     public function __construct($bdd)
     {
         $this->bdd = $bdd;
+    }
+
+    public function clear()
+    {
+       $this->firstName = "";
+       $this->lastName = "";
+       $this->email = "";
+       $this->password = "";
+       $this->id = "";
+    }
+
+    /**
+     * @param array $data
+     */
+    public function hydrate($data)
+    {
+        $this->clear();
+        $this->setId($data["id"]);
+        $this->setFirstName($data["firstname"]);
+        $this->setLastname($data["lastname"]);
+        $this->setEmail($data["email"]);
+        $this->setPassword($data["password"]);
     }
 
     public function addUser()
@@ -39,16 +65,16 @@ class UserManager
     /**
      * @param integer $id
      */
-    public function updateUser($id)
+    public function updateUser()
     {
-        $sql = "UPDATE " . self::TABLE . " SET firstname = '$this->firstName', lastname = '$this->lastName', email = '$this->email', password = '$this->password' where id = $id;";
+        $sql = "UPDATE " . self::TABLE . " SET firstname = '$this->firstName', lastname = '$this->lastName', email = '$this->email', password = '$this->password' where id = $this->id;";
         $this->bdd->execSql($sql);
     }
 
     /**
-     * @return mixed
+     * @return \mysqli_result
      */
-    public function listUser()
+    public function listUser():\mysqli_result
     {
         $sql = "SELECT * FROM " . self::TABLE;
         return $this->bdd->execSql($sql);
@@ -66,22 +92,30 @@ class UserManager
 
     /**
      * @param integer $id
-     * @return array|null
      */
     public function getUser($id)
     {
         $sql = "SELECT * FROM " . self::TABLE . " WHERE id = " . $id;
         $result = $this->bdd->execSql($sql);
-        return mysqli_fetch_assoc($result);
+        $data = mysqli_fetch_assoc($result);
+        $this->setFirstName($data["firstname"]);
+        $this->setLastName($data["lastname"]);
+        $this->setEmail($data["email"]);
+        $this->setPassword($data["password"]);
+        $this->setId($id);
     }
 
+    /**
+     * @param string $field
+     * @return string
+     */
     private function escapeField($field)
     {
         return mysqli_real_escape_string($this->bdd->getConnection(), $field);
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getFirstName()
     {
@@ -89,7 +123,7 @@ class UserManager
     }
 
     /**
-     * @param mixed $firstName
+     * @param string $firstName
      */
     public function setFirstName($firstName)
     {
@@ -97,7 +131,7 @@ class UserManager
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getLastName()
     {
@@ -105,7 +139,7 @@ class UserManager
     }
 
     /**
-     * @param mixed $lastName
+     * @param string $lastName
      */
     public function setLastName($lastName)
     {
@@ -113,7 +147,7 @@ class UserManager
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getEmail()
     {
@@ -121,7 +155,23 @@ class UserManager
     }
 
     /**
-     * @param mixed $email
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $email
      */
     public function setEmail($email)
     {
@@ -129,7 +179,7 @@ class UserManager
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getPassword()
     {
